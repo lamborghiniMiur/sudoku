64fa3865859524cea0a693e4d2501057c883d4e7
Tetsuya Miura <miura@trisystem.jp>
2020-07-06 09:43:15 +0900

    first java source

---
+++ src/main/java/jp/trisystem/sudoku/trisudoku/SudokuBean.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jp.trisystem.sudoku.trisudoku;
+
+import java.io.Serializable;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+/**
+ * 数独パズル.
+ *
+ * @author miura
+ */
+@Named
+@ViewScoped
+public class SudokuBean implements Serializable {
+    
+    
+}
